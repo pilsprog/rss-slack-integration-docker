43e32c6e06d44d74b94d3303e9a3563816c98e62
Snorre Magnus Davøen <snorremd@gmail.com>
2015-10-10 00:58:59 +0200

    Fix error in entrypoint.sh file
    entrypoint.sh copied template file to file with .coffee extension
    rather than .js extension. This caused the application to not find its
    config.
    
    Fixed.

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,6 @@ sed -e 's,${FEED},'"${FEED}"',g' \
     -e 's,${SLACK_BOT_USER},'"${SLACK_BOT_USER}"',g' \
     -i /usr/src/app/config.tmpl
 
-cp /usr/src/app/config.tmpl /usr/src/app/config.coffee
-
-cat /usr/src/app/node_modules/rss-slack-integration/package.json
+cp /usr/src/app/config.tmpl /usr/src/app/config.js
 
 exec "$@"
